5405c0de85f0f3f5ca0168e1da641f4629b6f230
Arbacynth <manicgaming5@gmail.com>
2022-06-29 14:32:59 +0100

    message

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+function headercolour(){
+    col = document.getElementById('col').style.color;
+    if (col == "cyan") {
+        document.getElementById("col").style.color = "green";
+    } 
+}
+
+var morb = new Audio();
+morb.src = "morb.mp3";
